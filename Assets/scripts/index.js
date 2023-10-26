4b4037649262c9390bd2576322aa3cf401247763
Javier Castro <109180080+JaCastro125@users.noreply.github.com>
2023-10-26 16:25:39 -0300

    Update index.js

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -38,9 +38,10 @@ fetch(Url)
             let segundoFiltro = filtrarCategoria(primerFiltro)
             pintarTarjetas(segundoFiltro)
         }
-    })
+    
     //.catch nos permite detener todo lo que se ejecuta en el .then en caso de que la 
     //informacion este corrupta
     .catch((error) => {
         console.log(error);
+        });
     });
